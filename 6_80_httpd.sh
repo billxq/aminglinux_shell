f3bfda10baa90e6d554e61eb46aa3e29daa9230d
billxq <178919347@qq.com>
2018-02-06 16:07:30 +0800

    time

--- 6_80_httpd.sh
+++ 6_80_httpd.sh
@@ -9,9 +9,9 @@
 # 由于本机没有httpd服务，所以改用transmission-daemon服务，端口为9091
 
 # ---------------------------------------------------------------
-d=`date +%F--%T`
 while true
 do
+d=`date +%F--%T`
 if netstat -lntp | grep -q ":9091 "  # -q表示quiet，不输出任何内容，9091后面有个空格，精确匹配，以防出现90912类似的端口
 then
 	echo "$d:transmission服务已开启！"
