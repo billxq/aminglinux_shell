63183cbc3ea2097ef284774236e63cf7ef3f4439
billxq <178919347@qq.com>
2018-03-05 18:37:44 +0800

    new 70

--- 70_cal.sh
+++ 70_cal.sh
@@ -4,31 +4,26 @@ is_int() {
 	n=`echo "$1" | sed 's/[0-9]//g'`
 	if [ ! -z $n ];then
 		echo "$1 is not a number!"
+        exit
 	fi
 }
 
 
-if is_int $1;then
-	exit
-fi
-
-if is_int $3;then
-	exit
-fi
-
+is_int $1
+is_int $3
 
 case $2 in
 	+)
-		echo "$1+$2"|bc
+		echo "$1+$3"|bc
 		;;
 	-)
-		echo "$1-$2"|bc
+		echo "$1-$3"|bc
 		;;
-	*)
-		echo "$1*$2"|bc
+	\*)
+		echo "$1*$3"|bc
 		;;
 	/)
-		echo "scale=2;$1/$2"|bc
+		echo "scale=2;$1/$3"|bc
 		;;
 	*)
 		echo "运算符错误！"
