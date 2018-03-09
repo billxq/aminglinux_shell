51881c669bfcb31bcd07b3803d70f5869fb4f0e7
billxq <178919347@qq.com>
2018-03-09 23:09:30 +0800

    rename

---
+++ name.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 去掉扩展名
+for i in `ls /etc/yum.repos.d/`;do
+    name=${i%.*}
+    echo $name
+done
+
+# 提取扩展名
+
+for i in `ls /etc/yum.repos.d/`;do
+    name=${i#*.}
+    echo $name
+done
+
+# 说明：%代表匹配位于%右侧的通配符，这里是.*,从右向左开始匹配
+# #号代表从左向右开始匹配
